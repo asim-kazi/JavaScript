3d65253fc9ffe2cf17f78cc0c3d60c67e7d61239
Asim Kazi <150136594+asim-kazi@users.noreply.github.com>
2024-12-07 17:13:53 +0000

    Conversions Practice

---
+++ Basics/variable.js
@@ -0,0 +1,19 @@
+// let a = "33"
+// let b = Number(a)
+// console.log(b);
+// console.log(typeof(b));
+
+// let a = 33
+// let b = String(a)
+// console.log(b);
+// console.log(typeof(b));
+
+// let a = 1
+// let b = Boolean(a)
+// console.log(b);
+// console.log(typeof(b));
+
+let a = true
+let b = Number(a)
+console.log(b);
+console.log(typeof(b));
