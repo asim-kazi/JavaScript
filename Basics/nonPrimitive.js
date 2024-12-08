7b865291e4789c5704b8eca614c2eeff01514b89
Asim Kazi <150136594+asim-kazi@users.noreply.github.com>
2024-12-08 11:16:24 +0000

    study about non primitive variables

---
+++ Basics/nonPrimitive.js
@@ -0,0 +1,24 @@
+//---------Array-----------
+// const name = ["Asim", "Mehfooz" , "Pranav"]
+// console.log(name);
+
+// console.log(name[0]);
+// console.log(name[1]);
+// console.log(name[2]);
+
+
+//--------Objects-----------
+// let myobj = {
+//     name : "asim",
+//     age : 20,
+//     CGPA : 8.5
+// }
+
+// console.log(myobj.name);
+
+
+//----------------Function-----------
+const myfunction = function() {
+    console.log("Hello World!!!!!1")
+}
+
