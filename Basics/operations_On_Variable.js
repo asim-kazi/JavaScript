c6c02b2db2de8542096adab5139cae23368f273a
Asim Kazi <150136594+asim-kazi@users.noreply.github.com>
2024-12-08 11:01:54 +0000

    perform operations on variable

---
+++ Basics/operations_On_Variable.js
@@ -0,0 +1,28 @@
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let greet = "Hello"
+// let name = " Asim"
+// let greeting = greet + name
+// console.log(greeting);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("2" + 2 + 2);
+// console.log(2 + 2 + "2");
+
+//-----------Increment And Decreament Operations--------------
+
+let a = 20
+++a
+console.log(a);
+
+let b = 20
+b++
+console.log(a);
