bf276497307aafe3d92cacef79433e4ee4cdf074
Asim Kazi <150136594+asim-kazi@users.noreply.github.com>
2024-12-09 12:47:43 +0000

    Study about Strings

---
+++ Basics/strings.js
@@ -0,0 +1,32 @@
+const name = "Asim"
+const age = 20
+console.log(`My Name Is ${name} And My Age Is ${age}`);
+
+const gameName = new String('asim-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   asim    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://asim.com/asim%20kazi"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('faruk'))
+
+console.log(gameName.split('-'));
