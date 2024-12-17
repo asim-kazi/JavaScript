bb26e1f0186cd493ecd7b1bfa56c042ce2aa3849
Asim Kazi <150136594+asim-kazi@users.noreply.github.com>
2024-12-17 18:19:08 +0000

    Study of all number function

---
+++ Basics/numberFunction.js
@@ -0,0 +1,16 @@
+const score = 100
+// console.log(score);
+
+const newNumber = new Number(100)
+// console.log(newNumber);
+// console.log(newNumber.toString().length);
+// console.log(newNumber.toFixed(2));
+// console.log(newNumber.toFixed(1));
+
+const otherNumber = 123.8945
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-In'));
+
