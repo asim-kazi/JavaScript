08e2a4c2f4a69d573c36bb63ef176f5318557637
Asim Kazi <150136594+asim-kazi@users.noreply.github.com>
2025-04-22 19:14:42 +0000

    Study of objects and array in function

--- Basics1/functions_01.js
+++ Basics1/functions_01.js
@@ -38,13 +38,65 @@ function sayMyName(){
 
 // console.log(loginUserMessage("Asim"));
 
-function loginUserMessage(username){
-    if(username === undefined){
-        console.log("Please enter UserName");
-        return
-    }
-        return `${username} Is Logged in...`
-    }
+// function loginUserMessage(username){
+//     if(username === undefined){
+//         console.log("Please enter UserName");
+//         return
+//     }
+//         return `${username} Is Logged in...`
+// }
     
-    console.log(loginUserMessage());
-    console.log(loginUserMessage("Asim"));
+    // console.log(loginUserMessage());
+    // console.log(loginUserMessage("Asim"));
+
+// function calculateCartPrice(num1){
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(2));
+
+function calculateCartPrice(...num1){       //(...)Three dots is known as rest operator and spread operator it is used when there are multiples numbers to (e.g : add to cart) 
+// rest operator is used when all items are separate separate and we want to combine them...
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,300,400));
+// console.log(calculateCartPrice(200,300,400,50,600));
+
+function calculateCartPrice(val1, val2, ...num1){       //Now when funtion call then first value is taken by val1 and 2nd value is taken by val2 and rest of the values are taken by num1...
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,300,400,50,600));
+
+
+//Use of objects in function 
+
+const user = {
+    username : "Asim",
+    price : 199,
+}
+
+function handleObjects(anyObject){
+    console.log(`Username is ${anyObject.username} and Price is  ${anyObject.price}`);
+}
+
+// handleObjects(user);
+// handleObjects({
+//     username : "Kazi",
+//     price : 299
+// });
+
+
+//Use of Arrays in Function
+
+const myNewArray = [200,300,400,500,600];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray
+));
+console.log(returnSecondValue([400,500,600]
+));
